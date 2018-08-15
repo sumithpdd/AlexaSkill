2f4e95476ab780140000bf079f0e490064c17664
sumith damodaran <sumith.damodaran@wearethink.com>
2018-08-15 16:26:53 +0100

    Error with intent name

--- AlexaSkill/Classes/AlexaIntents.cs
+++ AlexaSkill/Classes/AlexaIntents.cs
@@ -11,6 +11,6 @@
         public const string Whereistheorderintent = "WhereIsTheOrderIntent";
         public const string PickAWinnerintent = "PickAWinnerIntent";
         public const string ShowMyBasketintent = "ShowMyBasketIntent";
-        public const string AddProductToCartintent = "AddProductToCartintent";
+        public const string AddProductToCartintent = "AddProductToCartIntent";
     }
 }
